241511950fdd2283778d20f665075fd2e8273e00
Matteo DeSimone <thematteod@gmail.com>
2022-12-23 11:50:59 -0500

    Added error handling

--- API/Program.cs
+++ API/Program.cs
@@ -1,13 +1,5 @@
-using API.Interfaces;
-
-using API.Data;
-using Microsoft.EntityFrameworkCore;
-using API.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 using API.Extensions;
+using API.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,7 +12,10 @@ builder.Services.AddIdentityServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:4200"));
 
 app.UseAuthentication();
 app.UseAuthorization();
