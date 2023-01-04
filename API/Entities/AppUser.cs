f8c5994171c0b4516d5bd29068a252279d4aeb6e
Matteo DeSimone <thematteod@gmail.com>
2023-01-03 22:13:27 -0500

    Implemented messaging

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -19,5 +19,8 @@ namespace API.Entities
         
         public List<UserLike> LikeByUsers { get; set; }
         public List<UserLike> LikedUsers { get; set; }
+        
+        public List<Message> MessagesSent { get; set; }
+        public List<Message> MessagesReceived { get; set; }
     }
 }
